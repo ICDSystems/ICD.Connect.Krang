799ccffc41dcd3cfb5cc9c0f8ca3ad9c0f945e39
Chris Cameron <chris.cameron@wedoresi.com>
2017-08-25 13:51:28 -0400

    Implementing combine/uncombine features

--- ICD.Connect.Krang/Partitioning/PartitionManager.cs
+++ ICD.Connect.Krang/Partitioning/PartitionManager.cs
@@ -15,6 +15,7 @@ using ICD.Connect.Krang.Partitioning.Partitions;
 using ICD.Connect.Partitioning;
 using ICD.Connect.Partitioning.Controls;
 using ICD.Connect.Partitioning.Partitions;
+using ICD.Connect.Partitioning.Rooms;
 using ICD.Connect.Settings;
 using ICD.Connect.Settings.Core;
 
@@ -22,10 +23,17 @@ namespace ICD.Connect.Krang.Partitioning
 {
 	public sealed class PartitionManager : AbstractOriginator<PartitionManagerSettings>, IConsoleNode, IPartitionManager
 	{
+		/// <summary>
+		/// Raised when a parition control opens/closes.
+		/// </summary>
+		public event PartitionControlOpenStateCallback OnPartitionOpenStateChange;
+
 		private readonly PartitionsCollection m_Partitions;
 		private readonly IcdHashSet<IPartitionDeviceControl> m_SubscribedPartitions;
 
 		#region Properties
+
+		private static ICore Core { get { return ServiceProvider.GetService<ICore>(); } }
 
 		public PartitionsCollection Partitions { get { return m_Partitions; } }
 
@@ -54,6 +62,17 @@ namespace ICD.Connect.Krang.Partitioning
 			ServiceProvider.AddService<IPartitionManager>(this);
 		}
 
+		/// <summary>
+		/// Override to release resources.
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void DisposeFinal(bool disposing)
+		{
+			OnPartitionOpenStateChange = null;
+
+			base.DisposeFinal(disposing);
+		}
+
 		#region Controls
 
 		/// <summary>
@@ -72,6 +91,313 @@ namespace ICD.Connect.Krang.Partitioning
 
 			return ServiceProvider.GetService<ICore>()
 			                      .GetControl<IPartitionDeviceControl>(partition.PartitionControl);
+		}
+
+		#endregion
+
+		#region Combine
+
+		/// <summary>
+		/// Gets the combine room containing the given partition.
+		/// </summary>
+		/// <param name="partition"></param>
+		/// <returns></returns>
+		public IRoom GetCombineRoom(IPartition partition)
+		{
+			if (partition == null)
+				throw new ArgumentNullException("partition");
+
+			return GetRooms().FirstOrDefault(r => r.Partitions.ContainsRecursive(partition.Id));
+		}
+
+		/// <summary>
+		/// Returns combine rooms and any individual rooms that are not part of a combined space.
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<IRoom> GetTopLevelRooms()
+		{
+			IRoom[] rooms = GetRooms().OrderByDescending(r => r.Partitions.Count).ToArray();
+			IcdHashSet<IRoom> visited = new IcdHashSet<IRoom>();
+
+			foreach (IRoom room in rooms)
+			{
+				if (visited.Contains(room))
+					continue;
+
+				visited.Add(room);
+				visited.AddRange(room.GetRoomsRecursive());
+
+				yield return room;
+			}
+		}
+
+		/// <summary>
+		/// Toggles the given partition to create a new combine room or uncombine an existing room.
+		/// </summary>
+		/// <typeparam name="TRoom"></typeparam>
+		/// <param name="partition"></param>
+		/// <param name="func"></param>
+		public void ToggleCombineRooms<TRoom>(IPartition partition, Func<TRoom> func) where TRoom : IRoom
+		{
+			IRoom room = GetCombineRoom(partition);
+
+			if (room == null)
+				CombineRooms(partition, func);
+			else
+				UncombineRooms(partition, func);
+		}
+
+		/// <summary>
+		/// Creates a new room instance, or expands an existing room instance, to contain the given partitions.
+		/// </summary>
+		/// <typeparam name="TRoom"></typeparam>
+		/// <param name="partitions"></param>
+		/// <param name="constructor"></param>
+		public void CombineRooms<TRoom>(IEnumerable<IPartition> partitions, Func<TRoom> constructor)
+			where TRoom : IRoom
+		{
+			if (partitions == null)
+				throw new ArgumentNullException("partitions");
+
+			if (constructor == null)
+				throw new ArgumentNullException("constructor");
+
+			foreach (IPartition partition in partitions)
+				CombineRooms(partition, constructor);
+		}
+
+		/// <summary>
+		/// Creates a new room instance, or expands an existing room instance, to contain the given partition.
+		/// </summary>
+		/// <typeparam name="TRoom"></typeparam>
+		/// <param name="partition"></param>
+		/// <param name="constructor"></param>
+		public void CombineRooms<TRoom>(IPartition partition, Func<TRoom> constructor)
+			where TRoom : IRoom
+		{
+			if (partition == null)
+				throw new ArgumentNullException("partition");
+
+			if (constructor == null)
+				throw new ArgumentNullException("constructor");
+
+			// Partition doesn't actually join any rooms
+			if (partition.RoomsCount <= 1)
+				return;
+
+			IRoom[] rooms = GetAdjacentCombineRooms(partition).ToArray();
+
+			// Get the complete set of partitions through the new combine space
+			IEnumerable<IPartition> partitions = rooms.SelectMany(r => r.Partitions.GetInstancesRecursive())
+													  .Append(partition)
+													  .Distinct();
+
+			DestroyCombineRooms(rooms);
+			CreateCombineRoom(partitions, constructor);
+		}
+
+		/// <summary>
+		/// Removes the partitions from existing rooms.
+		/// </summary>
+		/// <param name="partitions"></param>
+		/// <param name="constructor"></param>
+		public void UncombineRooms<TRoom>(IEnumerable<IPartition> partitions, Func<TRoom> constructor)
+			where TRoom : IRoom
+		{
+			if (partitions == null)
+				throw new ArgumentNullException("partitions");
+
+			if (constructor == null)
+				throw new ArgumentNullException("constructor");
+
+			foreach (IPartition partition in partitions)
+				UncombineRooms(partition, constructor);
+		}
+
+		/// <summary>
+		/// Removes the partition from existing rooms.
+		/// </summary>
+		/// <param name="partition"></param>
+		/// <param name="constructor"></param>
+		public void UncombineRooms<TRoom>(IPartition partition, Func<TRoom> constructor)
+			where TRoom : IRoom
+		{
+			if (partition == null)
+				throw new ArgumentNullException("partition");
+
+			if (constructor == null)
+				throw new ArgumentNullException("constructor");
+
+			// Partition doesn't actually join any rooms
+			if (partition.RoomsCount <= 1)
+				return;
+
+			// Take rooms as an array, since we will potentially modify the core rooms collection
+			IRoom[] rooms = GetRooms().ToArray();
+
+			foreach (IRoom room in rooms)
+				UncombineRoom(room, partition, constructor);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Removes the rooms from the core and disposes them.
+		/// </summary>
+		/// <param name="rooms"></param>
+		private void DestroyCombineRooms(IEnumerable<IRoom> rooms)
+		{
+			if (rooms == null)
+				throw new ArgumentNullException("rooms");
+
+			foreach (IRoom room in rooms)
+				DestroyCombineRoom(room);
+		}
+
+		/// <summary>
+		/// Removes the room from the core and disposes it.
+		/// </summary>
+		/// <param name="room"></param>
+		private void DestroyCombineRoom(IRoom room)
+		{
+			if (room == null)
+				throw new ArgumentNullException("room");
+
+			Logger.AddEntry(eSeverity.Informational, "{0} destroying combined room {1}", this, room);
+
+			Core.Originators.RemoveChild(room);
+			IDisposable disposable = room as IDisposable;
+			if (disposable != null)
+				disposable.Dispose();
+		}
+
+		/// <summary>
+		/// Creates a new room of the given type containing the given partitions.
+		/// </summary>
+		/// <typeparam name="TRoom"></typeparam>
+		/// <param name="partitions"></param>
+		/// <param name="constructor"></param>
+		private void CreateCombineRoom<TRoom>(IEnumerable<IPartition> partitions, Func<TRoom> constructor)
+			where TRoom : IRoom
+		{
+			if (partitions == null)
+				throw new ArgumentNullException("partitions");
+
+			if (constructor == null)
+				throw new ArgumentNullException("constructor");
+
+			TRoom room = constructor();
+			room.Partitions.AddRange(partitions.Select(p => p.Id));
+
+			Core.Originators.AddChildAssignId(room);
+
+			Logger.AddEntry(eSeverity.Informational, "{0} created new combine room {1}", this, room);
+		}
+
+		/// <summary>
+		/// Destroys the room, creating new combine rooms where necessary (e.g. splitting 4 rooms in half)
+		/// </summary>
+		/// <typeparam name="TRoom"></typeparam>
+		/// <param name="room"></param>
+		/// <param name="partition"></param>
+		/// <param name="constructor"></param>
+		private void UncombineRoom<TRoom>(IRoom room, IPartition partition, Func<TRoom> constructor)
+			where TRoom : IRoom
+		{
+			if (room == null)
+				throw new ArgumentNullException("room");
+
+			if (partition == null)
+				throw new ArgumentNullException("partition");
+
+			if (constructor == null)
+				throw new ArgumentNullException("constructor");
+
+			if (!room.Partitions.ContainsRecursive(partition.Id))
+				return;
+
+			IEnumerable<IPartition[]> split = SplitPartitions(room, partition);
+
+			// Destroy the combine room
+			DestroyCombineRoom(room);
+
+			// Build the new combine rooms
+			foreach (IPartition[] group in split)
+				CombineRooms(group, constructor);
+		}
+
+		/// <summary>
+		/// Returns groups of partitions that result from splitting on the given partition.
+		/// E.g. AB-BC-CD being split on BC would result in [AB], [CD].
+		/// </summary>
+		/// <param name="room"></param>
+		/// <param name="partition"></param>
+		/// <returns></returns>
+		private IEnumerable<IPartition[]> SplitPartitions(IRoom room, IPartition partition)
+		{
+			if (room == null)
+				throw new ArgumentNullException("room");
+
+			if (partition == null)
+				throw new ArgumentNullException("partition");
+
+			IPartition[] partitions = room.Partitions
+			                              .GetInstancesRecursive()
+			                              .Except(partition)
+										  .Distinct()
+			                              .ToArray();
+
+			return m_Partitions.SplitAdjacentPartitionsByPartition(partitions, partition);
+		}
+
+		/// <summary>
+		/// Loops through the rooms to find combine rooms that are adjacent to the given partition.
+		/// </summary>
+		/// <param name="partition"></param>
+		/// <returns></returns>
+		private static IEnumerable<IRoom> GetAdjacentCombineRooms(IPartition partition)
+		{
+			if (partition == null)
+				throw new ArgumentNullException("partition");
+
+			return GetRooms().Where(room => room.IsCombineRoom() && IsCombineRoomAdjacent(room, partition));
+		}
+
+		/// <summary>
+		/// Returns true if the given combined space is adjacent to the given partition.
+		/// Returns false if the given combined space contains the given partition.
+		/// </summary>
+		/// <param name="room"></param>
+		/// <param name="partition"></param>
+		/// <returns></returns>
+		private static bool IsCombineRoomAdjacent(IRoom room, IPartition partition)
+		{
+			if (room == null)
+				throw new ArgumentNullException("room");
+
+			if (partition == null)
+				throw new ArgumentNullException("partition");
+
+			// Only interested in the edge of the combined space
+			if (room.Partitions.ContainsRecursive(partition.Id))
+				return false;
+
+			// Returns true if any of the rooms in the combined space overlap with the partition rooms.
+			return room.Partitions
+			           .GetInstancesRecursive()
+			           .Any(p => partition.GetRooms()
+			                              .Any(p.ContainsRoom));
+		}
+
+		/// <summary>
+		/// Gets the rooms available to the core.
+		/// </summary>
+		/// <returns></returns>
+		private static IEnumerable<IRoom> GetRooms()
+		{
+			return Core.Originators.GetChildren<IRoom>();
 		}
 
 		#endregion
@@ -142,7 +468,9 @@ namespace ICD.Connect.Krang.Partitioning
 		/// <param name="args"></param>
 		private void PartitionOnOpenStatusChanged(object sender, BoolEventArgs args)
 		{
-			throw new NotImplementedException();
+			PartitionControlOpenStateCallback handler = OnPartitionOpenStateChange;
+			if (handler != null)
+				handler(sender as IPartitionDeviceControl, args.Data);
 		}
 
 		#endregion
--- ICD.Connect.Krang/Partitioning/Partitions/PartitionsCollection.cs
+++ ICD.Connect.Krang/Partitioning/Partitions/PartitionsCollection.cs
@@ -7,6 +7,7 @@ using ICD.Common.Utils.Collections;
 using ICD.Common.Utils.Extensions;
 using ICD.Connect.Devices.Controls;
 using ICD.Connect.Partitioning.Partitions;
+using ICD.Connect.Partitioning.Rooms;
 
 namespace ICD.Connect.Krang.Partitioning.Partitions
 {
@@ -19,6 +20,8 @@ namespace ICD.Connect.Krang.Partitioning.Partitions
 
 		private readonly Dictionary<int, IPartition> m_Partitions;
 		private readonly Dictionary<DeviceControlInfo, IcdHashSet<IPartition>> m_ControlPartitions;
+		private readonly Dictionary<int, IcdHashSet<IPartition>> m_RoomAdjacentPartitions;
+
 		private readonly SafeCriticalSection m_PartitionsSection;
 
 		private readonly PartitionManager m_Manager;
@@ -37,9 +40,10 @@ namespace ICD.Connect.Krang.Partitioning.Partitions
 			m_Manager = manager;
 
 			m_Partitions = new Dictionary<int, IPartition>();
-			m_PartitionsSection = new SafeCriticalSection();
-
+			m_RoomAdjacentPartitions = new Dictionary<int, IcdHashSet<IPartition>>();
 			m_ControlPartitions = new Dictionary<DeviceControlInfo, IcdHashSet<IPartition>>();
+
+			m_PartitionsSection = new SafeCriticalSection();
 		}
 
 		#region Methods
@@ -87,6 +91,149 @@ namespace ICD.Connect.Krang.Partitioning.Partitions
 			OnPartitionsChanged.Raise(this);
 		}
 
+		/// <summary>
+		/// Gets the partitions related to the given control.
+		/// </summary>
+		/// <param name="deviceControlInfo"></param>
+		public IEnumerable<IPartition> GetPartitions(DeviceControlInfo deviceControlInfo)
+		{
+			m_PartitionsSection.Enter();
+
+			try
+			{
+				return m_ControlPartitions.ContainsKey(deviceControlInfo)
+					       ? m_ControlPartitions[deviceControlInfo]
+					       : Enumerable.Empty<IPartition>();
+			}
+			finally
+			{
+				m_PartitionsSection.Leave();
+			}
+		}
+
+		/// <summary>
+		/// Gets the immediately adjacent partitions for the given partition.
+		/// </summary>
+		/// <param name="partition"></param>
+		/// <returns></returns>
+		public IEnumerable<IPartition> GetAdjacentPartitions(IPartition partition)
+		{
+			m_PartitionsSection.Enter();
+
+			try
+			{
+				return partition.GetRooms()
+				                .SelectMany(r => GetRoomAdjacentPartitions(r))
+				                .Except(partition)
+				                .Distinct();
+			}
+			finally
+			{
+				m_PartitionsSection.Leave();
+			}
+		}
+
+		/// <summary>
+		/// Gets the partitions adjacent to the given room.
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<IPartition> GetRoomAdjacentPartitions(IRoom room)
+		{
+			if (room == null)
+				throw new ArgumentNullException("room");
+
+			return GetRoomAdjacentPartitions(room.Id);
+		}
+
+		/// <summary>
+		/// Gets the partitions adjacent to the given room.
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<IPartition> GetRoomAdjacentPartitions(int roomId)
+		{
+			m_PartitionsSection.Enter();
+
+			try
+			{
+				return m_RoomAdjacentPartitions.ContainsKey(roomId)
+					       ? m_RoomAdjacentPartitions[roomId].ToArray()
+					       : Enumerable.Empty<IPartition>();
+			}
+			finally
+			{
+				m_PartitionsSection.Leave();
+			}
+		}
+
+		/// <summary>
+		/// Given a sequence of partitions and a partition to split on, returns the remaining congruous groups of adjacent partitions.
+		/// </summary>
+		/// <param name="partitions"></param>
+		/// <param name="split"></param>
+		/// <returns></returns>
+		public IEnumerable<IPartition[]> SplitAdjacentPartitionsByPartition(IEnumerable<IPartition> partitions, IPartition split)
+		{
+			// Unique partitions except the split
+			IPartition[] partitionsArray = partitions.Except(split)
+			                                         .Distinct()
+			                                         .ToArray();
+
+			// First build a map of how the partitions are adjacent to each other.
+			Dictionary<IPartition, IcdHashSet<IPartition>> adjacency = new Dictionary<IPartition, IcdHashSet<IPartition>>();
+			
+			foreach (IPartition partition in partitionsArray)
+			{
+				// Workaround for compiler warning
+				IPartition localEnclosurePartition = partition;
+
+				IcdHashSet<IPartition> adjacent = partitionsArray.Except(partition)
+				                                                 .Where(p => p.IsAdjacent(localEnclosurePartition))
+				                                                 .ToHashSet();
+				adjacency.Add(partition, adjacent);
+			}
+
+			// Loop over the keys and find groups
+			Dictionary<IPartition, IcdHashSet<IPartition>> groups = new Dictionary<IPartition, IcdHashSet<IPartition>>();
+			RecurseAdjacencyMap(adjacency, (root, node) =>
+			                               {
+											   if (!groups.ContainsKey(root))
+												   groups.Add(root, new IcdHashSet<IPartition>());
+				                               groups[root].Add(node);
+			                               });
+
+			return groups.Values.Select(v => v.ToArray());
+		}
+
+		/// <summary>
+		/// Loops through the map calling the callback for each distinct node.
+		/// </summary>
+		/// <param name="map"></param>
+		/// <param name="rootAndNodeCallback"></param>
+		private static void RecurseAdjacencyMap(IDictionary<IPartition, IcdHashSet<IPartition>> map,
+		                                 Action<IPartition, IPartition> rootAndNodeCallback)
+		{
+			Queue<IPartition> processing = new Queue<IPartition>();
+			IcdHashSet<IPartition> visited = new IcdHashSet<IPartition>();
+
+			foreach (IPartition root in map.Keys)
+			{
+				processing.Enqueue(root);
+
+				while (processing.Count > 0)
+				{
+					IPartition node = processing.Dequeue();
+					if (visited.Contains(node))
+						continue;
+
+					visited.Add(node);
+
+					rootAndNodeCallback(root, node);
+
+					processing.EnqueueRange(map[node]);
+				}
+			}
+		}
+
 		#endregion
 
 		/// <summary>
@@ -98,8 +245,21 @@ namespace ICD.Connect.Krang.Partitioning.Partitions
 
 			try
 			{
+				m_RoomAdjacentPartitions.Clear();
 				m_ControlPartitions.Clear();
 
+				// Build room adjacency lookup
+				foreach (IPartition partition in m_Partitions.Values)
+				{
+					foreach (int room in partition.GetRooms())
+					{
+						if (!m_RoomAdjacentPartitions.ContainsKey(room))
+							m_RoomAdjacentPartitions.Add(room, new IcdHashSet<IPartition>());
+						m_RoomAdjacentPartitions[room].Add(partition);
+					}
+				}
+
+				// Build control to partition lookup
 				foreach (IPartition partition in m_Partitions.Values.Where(p => p.HasPartitionControl()))
 				{
 					if (!m_ControlPartitions.ContainsKey(partition.PartitionControl))
