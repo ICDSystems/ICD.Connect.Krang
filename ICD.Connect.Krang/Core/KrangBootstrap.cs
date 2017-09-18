61c9e992cfc7f72e2e001bb4f955b2e792ac1dbd
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-18 11:47:24 -0400

    Tidying

--- ICD.Connect.Krang/Core/KrangBootstrap.cs
+++ ICD.Connect.Krang/Core/KrangBootstrap.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using ICD.Common.Logging.Console;
 using ICD.Common.Logging.Console.Loggers;
 using ICD.Common.Permissions;
