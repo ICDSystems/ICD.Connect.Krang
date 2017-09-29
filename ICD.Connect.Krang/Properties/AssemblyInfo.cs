530c0b01ae92bda272076f91714123035e82cc2f
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-29 10:41:56 -0400

    Updating AssemblyInfo

--- ICD.Connect.Krang/Properties/AssemblyInfo.cs
+++ ICD.Connect.Krang/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("ICD.SimplSharp.KrangLib")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ICD.SimplSharp.KrangLib")]
+[assembly: AssemblyTitle("ICD.Connect.Krang")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Krang")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
 [assembly: AssemblyVersion("1.0.1.*")]
