5b98e71ad750b5be4ca953dbf98567ef36d3089f
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-08 12:07:33 -0500

    Adding console commands to SimplRoom

--- ICD.Connect.Krang/Rooms/SimplRoom.cs
+++ ICD.Connect.Krang/Rooms/SimplRoom.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using ICD.Common.Utils;
 using ICD.Common.Utils.Extensions;
+using ICD.Connect.API.Commands;
+using ICD.Connect.API.Nodes;
 using ICD.Connect.Partitioning.Rooms;
 using ICD.Connect.Settings.Core;
 
@@ -164,6 +166,67 @@ namespace ICD.Connect.Krang.Rooms
 			base.ApplySettingsFinal(settings, factory);
 
 			SetCrosspoints(settings.GetCrosspoints());
+		}
+
+		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Calls the delegate for each console status item.
+		/// </summary>
+		/// <param name="addRow"></param>
+		public override void BuildConsoleStatus(AddStatusRowDelegate addRow)
+		{
+			base.BuildConsoleStatus(addRow);
+
+			addRow("Volume Level", m_VolumeLevelFeedback);
+			addRow("Volume Mute", m_VolumeMuteFeedback);
+		}
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			yield return new ConsoleCommand("PrintCrosspoints", "Prints the crosspoints added to the room", () => PrintCrosspoints());
+			yield return new GenericConsoleCommand<ushort>("SetVolumeLevel", "SetVolumeLevel <LEVEL>", l => SetVolumeLevel(l));
+		}
+
+		/// <summary>
+		/// Workaround for "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		/// <summary>
+		/// Returns a table of the crosspoints in the room.
+		/// </summary>
+		/// <returns></returns>
+		private string PrintCrosspoints()
+		{
+			m_CrosspointsSection.Enter();
+
+			try
+			{
+				TableBuilder builder = new TableBuilder("Id", "Type");
+
+				foreach (KeyValuePair<ushort, eCrosspointType> kvp in m_Crosspoints)
+					builder.AddRow(kvp.Key, kvp.Value);
+
+				return builder.ToString();
+			}
+			finally
+			{
+				m_CrosspointsSection.Leave();
+			}
 		}
 
 		#endregion
