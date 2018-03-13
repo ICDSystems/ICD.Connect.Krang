d665e81fd5ac7826a3e5c8a14ccef222a8d90292
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-12 21:42:11 -0400

    Tidying

--- ICD.Connect.Core/Program.cs
+++ ICD.Connect.Core/Program.cs
@@ -7,23 +7,35 @@ using ICD.Connect.Krang.Core;
 namespace ICD.Connect.Core
 {
 	internal static class Program
-    {
-        private static KrangBootstrap s_Bootstrap;
-        
-	    public static void Main()
-        {
-            s_Bootstrap = new KrangBootstrap();
-            AssemblyLoadContext.Default.Unloading += context => s_Bootstrap.Stop();
-            s_Bootstrap.Start();
+	{
+		private static readonly KrangBootstrap s_Bootstrap;
 
-	        while (true)
-	        {
-		        string command = Console.ReadLine();
-                if (command.Equals("exit", StringComparison.OrdinalIgnoreCase))
-                    break;
-		        ApiConsole.ExecuteCommand(command);
-	        }
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		static Program()
+		{
+			s_Bootstrap = new KrangBootstrap();
+
+			AssemblyLoadContext.Default.Unloading += context => s_Bootstrap.Stop();
 		}
-    }
+
+		public static void Main()
+		{
+			s_Bootstrap.Start();
+
+			while (true)
+			{
+				string command = Console.ReadLine();
+				if (command == null)
+					continue;
+
+				if (command.Equals("exit", StringComparison.OrdinalIgnoreCase))
+					break;
+
+				ApiConsole.ExecuteCommand(command);
+			}
+		}
+	}
 }
 #endif
