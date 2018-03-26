e3144240670d81cb837f75bffe534b9ecc7de254
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-26 14:17:42 -0400

    Tidying

---
+++ ICD.Connect.Krang/Core/ApiOriginatorsNodeGroup.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using ICD.Connect.API.Nodes;
+using ICD.Connect.Settings;
+
+namespace ICD.Connect.Krang.Core
+{
+	public sealed class ApiOriginatorsNodeGroup<TOriginator> : AbstractApiNodeGroup
+		where TOriginator : IOriginator
+	{
+		private readonly IOriginatorCollection<IOriginator> m_Originators;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="originators"></param>
+		public ApiOriginatorsNodeGroup(IOriginatorCollection<IOriginator> originators)
+		{
+			m_Originators = originators;
+		}
+
+		public override object this[uint key] { get { return m_Originators.GetChild<TOriginator>((int)key); } }
+
+		public override bool ContainsKey(uint key)
+		{
+			return m_Originators.ContainsChild<TOriginator>((int)key);
+		}
+
+		public override IEnumerable<KeyValuePair<uint, object>> GetKeyedNodes()
+		{
+			return m_Originators.GetChildren<TOriginator>().Select(o => new KeyValuePair<uint, object>((uint)o.Id, o));
+		}
+	}
+}
