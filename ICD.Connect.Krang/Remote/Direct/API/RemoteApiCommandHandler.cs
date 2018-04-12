5a0738a1d069483cef244cea9ca322f4d8fa55f5
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-12 13:36:45 -0400

    feat: Temp implementation of API event feedback handling

--- ICD.Connect.Krang/Remote/Direct/API/RemoteApiCommandHandler.cs
+++ ICD.Connect.Krang/Remote/Direct/API/RemoteApiCommandHandler.cs
@@ -1,14 +1,34 @@
-﻿using ICD.Connect.API;
+﻿using ICD.Common.Utils;
+using ICD.Common.Utils.Json;
+using ICD.Connect.API;
+using ICD.Connect.API.Info;
 using ICD.Connect.Protocol.Network.Direct;
 
 namespace ICD.Connect.Krang.Remote.Direct.API
 {
-	public sealed class RemoteApiCommandHandler : AbstractMessageHandler<RemoteApiMessage>
+	/// <summary>
+	/// The RemoteApiCommandHandler receives 
+	/// </summary>
+	public sealed class RemoteApiCommandHandler : AbstractMessageHandler<RemoteApiMessage>, IApiRequestor
 	{
+		/// <summary>
+		/// Handles the message receieved
+		/// </summary>
+		/// <param name="message"></param>
+		/// <returns>Returns an AbstractMessage as a reply, or null for no reply</returns>
 		protected override AbstractMessage HandleMessage(RemoteApiMessage message)
 		{
-			ApiHandler.HandleRequest(message.Command);
+			ApiHandler.HandleRequest(this, message.Command);
 			return new RemoteApiReply {Command = message.Command};
+		}
+
+		/// <summary>
+		/// Handles an event feedback command from the ApiHandler.
+		/// </summary>
+		/// <param name="command"></param>
+		public void HandleFeedback(ApiClassInfo command)
+		{
+			IcdConsole.PrintLine(eConsoleColor.Magenta, "API event raised - {0}", JsonUtils.Format(command));
 		}
 	}
 }
