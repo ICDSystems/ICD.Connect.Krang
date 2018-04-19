6e5d939698dc611348929ebc70d2836585cf0836
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-19 15:59:52 -0400

    feat: Adding informational version to AssemblyInfo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [Unreleased]
 ### Added
  - Added VersionInfo console command to KrangBootstrap
+ - Added informational version info to AssemblyInfo
 
 ## [3.1.0] - 2018-04-15
 
--- ICD.Connect.Krang/Properties/AssemblyInfo.cs
+++ ICD.Connect.Krang/Properties/AssemblyInfo.cs
@@ -5,3 +5,8 @@ using System.Reflection;
 [assembly: AssemblyProduct("ICD.Connect.Krang")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("3.1.0.0")]
+
+[assembly: System.Reflection.AssemblyInformationalVersion("2.0.0")]
+#if SIMPLSHARP
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("2.0.0")]
+#endif
