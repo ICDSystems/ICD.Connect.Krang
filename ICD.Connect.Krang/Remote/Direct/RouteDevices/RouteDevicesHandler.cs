fc6ee45766c949b8de3f7024b1ac15e13b4d813d
Chris Cameron <chris.cameron@wedoresi.com>
2018-04-23 17:05:20 -0400

    fix: Multi-routing more resistant to routing graph being reloaded

--- ICD.Connect.Krang/Remote/Direct/RouteDevices/RouteDevicesHandler.cs
+++ ICD.Connect.Krang/Remote/Direct/RouteDevices/RouteDevicesHandler.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using ICD.Common.Properties;
 using ICD.Common.Utils;
 using ICD.Common.Utils.Services;
 using ICD.Connect.Protocol.Network.Direct;
 using ICD.Connect.Routing.EventArguments;
 using ICD.Connect.Routing.Extensions;
+using ICD.Connect.Routing.RoutingGraphs;
 using ICD.Connect.Settings.Core;
 
 namespace ICD.Connect.Krang.Remote.Direct.RouteDevices
@@ -16,24 +19,82 @@ namespace ICD.Connect.Krang.Remote.Direct.RouteDevices
 		private readonly List<RouteDevicesMessage> m_PendingMessages;
 		private readonly SafeCriticalSection m_PendingMessagesSection;
 
+		private IRoutingGraph m_SubscribedRoutingGraph;
+
 		/// <summary>
 		/// Constructor.
 		/// </summary>
 		public RouteDevicesHandler()
 		{
-			m_Core = ServiceProvider.GetService<ICore>();
-			m_Core.GetRoutingGraph().OnRouteFinished += RoutingGraphOnOnRouteFinished;
-
 			m_PendingMessages = new List<RouteDevicesMessage>();
 			m_PendingMessagesSection = new SafeCriticalSection();
+
+			m_Core = ServiceProvider.GetService<ICore>();
+			m_Core.Originators.OnChildrenChanged += OriginatorsOnChildrenChanged;
+		}
+
+		/// <summary>
+		/// Release resources.
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+
+			SetRoutingGraph(null);
+		}
+
+		private void OriginatorsOnChildrenChanged(object sender, EventArgs eventArgs)
+		{
+			IRoutingGraph graph = GetRoutingGraph();
+			SetRoutingGraph(graph);
+		}
+
+		private void SetRoutingGraph(IRoutingGraph graph)
+		{
+			if (graph == m_SubscribedRoutingGraph)
+				return;
+
+			Unsubscribe(m_SubscribedRoutingGraph);
+			m_SubscribedRoutingGraph = graph;
+			Subscribe(m_SubscribedRoutingGraph);
+		}
+
+		[CanBeNull]
+		private IRoutingGraph GetRoutingGraph()
+		{
+			IRoutingGraph output;
+			m_Core.TryGetRoutingGraph(out output);
+			return output;
 		}
 
 		protected override RouteDevicesReply HandleMessage(RouteDevicesMessage message)
 		{
 			m_PendingMessagesSection.Execute(() => m_PendingMessages.Add(message));
-			m_Core.GetRoutingGraph().Route(message.Operation);
+
+			IRoutingGraph graph = m_SubscribedRoutingGraph;
+			if (graph != null)
+				graph.Route(message.Operation);
 
 			return null;
+		}
+
+		#region Routing Graph Callbacks
+
+		private void Subscribe(IRoutingGraph graph)
+		{
+			if (graph == null)
+				return;
+
+			graph.OnRouteFinished += RoutingGraphOnOnRouteFinished;
+		}
+
+		private void Unsubscribe(IRoutingGraph graph)
+		{
+			if (graph == null)
+				return;
+
+			graph.OnRouteFinished -= RoutingGraphOnOnRouteFinished;
 		}
 
 		private void RoutingGraphOnOnRouteFinished(object sender, RouteFinishedEventArgs args)
@@ -64,5 +125,7 @@ namespace ICD.Connect.Krang.Remote.Direct.RouteDevices
 
 			RaiseReply(reply);
 		}
+
+		#endregion
 	}
 }
