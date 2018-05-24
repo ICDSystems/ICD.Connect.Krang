da6127a405dc63afc9f41d32597af4fd4109e554
Drew Tingen <drew.tingen@icdsystems.com>
2018-05-24 10:04:21 -0400

    feat: Adding OnKrangCleared event to SPlusOnKrangLoadedEventRelay

--- ICD.Connect.Krang/SPlusShims/SPlusOnKrangLoadedEventRelay.cs
+++ ICD.Connect.Krang/SPlusShims/SPlusOnKrangLoadedEventRelay.cs
@@ -6,20 +6,28 @@ namespace ICD.Connect.Krang.SPlusShims
 	public sealed class SPlusOnKrangLoadedEventRelay : IDisposable
 	{
 		public event EventHandler OnKrangLoaded;
+		public event EventHandler OnKrangCleared;
 
 		public SPlusOnKrangLoadedEventRelay()
 		{
 			SPlusKrangBootstrap.OnKrangLoaded += SPlusKrangBootstrapOnKrangLoaded;
+			SPlusKrangBootstrap.OnKrangCleared += SPlusKrangBootstrapOnKrangCleared;
 		}
 
 		public void Dispose()
 		{
 			SPlusKrangBootstrap.OnKrangLoaded -= SPlusKrangBootstrapOnKrangLoaded;
+			SPlusKrangBootstrap.OnKrangCleared -= SPlusKrangBootstrapOnKrangCleared;
 		}
 
 		private void SPlusKrangBootstrapOnKrangLoaded(object sender, EventArgs eventArgs)
 		{
 			OnKrangLoaded.Raise(this);
+		}
+
+		private void SPlusKrangBootstrapOnKrangCleared(object sender, EventArgs eventArgs)
+		{
+			OnKrangCleared.Raise(this);
 		}
 	}
 }
