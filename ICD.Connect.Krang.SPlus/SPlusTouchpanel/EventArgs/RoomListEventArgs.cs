890b199c36881cad146461c8ac13eec6352038ef
Drew Tingen <drew.tingen@icdsystems.com>
2018-12-12 12:05:36 -0500

    fix: Seal class where appropriate

--- ICD.Connect.Krang.SPlus/SPlusTouchpanel/EventArgs/RoomListEventArgs.cs
+++ ICD.Connect.Krang.SPlus/SPlusTouchpanel/EventArgs/RoomListEventArgs.cs
@@ -5,7 +5,7 @@ using ICD.Connect.Krang.SPlus.SPlusTouchpanel.Proxy;
 
 namespace ICD.Connect.Krang.SPlus.SPlusTouchpanel.EventArgs
 {
-	public class RoomListEventArgs : AbstractGenericApiEventArgs<IEnumerable<KeyValuePair<ushort, RoomInfo>>>
+	public sealed class RoomListEventArgs : AbstractGenericApiEventArgs<IEnumerable<KeyValuePair<ushort, RoomInfo>>>
 	{
 		public RoomListEventArgs(IEnumerable<KeyValuePair<ushort, RoomInfo>> roomList)
 			: base(SPlusTouchpanelDeviceApi.EVENT_ROOM_LIST, roomList)
