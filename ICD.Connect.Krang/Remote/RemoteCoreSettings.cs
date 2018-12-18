aa7d519cb71ee0c75b27466c69a96ad29a00ffe7
Chris Cameron <chris.cameron@wedoresi.com>
2018-12-18 06:34:30 -0500

    feat: Implementing ICoreSettings.GetHeader method

--- ICD.Connect.Krang/Remote/RemoteCoreSettings.cs
+++ ICD.Connect.Krang/Remote/RemoteCoreSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using ICD.Connect.Settings;
 using ICD.Connect.Settings.Core;
+using ICD.Connect.Settings.Header;
 
 namespace ICD.Connect.Krang.Remote
 {
@@ -11,5 +12,15 @@ namespace ICD.Connect.Krang.Remote
 		public override Type OriginatorType { get { return typeof(RemoteCore); } }
 
 		public override SettingsCollection OriginatorSettings { get { return null; } }
+
+		/// <summary>
+		/// Parses and returns only the header portion from the full XML config.
+		/// </summary>
+		/// <param name="configXml"></param>
+		/// <returns></returns>
+		public override ConfigurationHeader GetHeader(string configXml)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
