f8ea0901f19bdf9e625f19ee9143fca7e76ca0ed
Chris Cameron <chris.cameron@wedoresi.com>
2019-01-30 20:42:18 -0500

    fix: Setting host info on RemoteApiReply

--- ICD.Connect.Krang/Remote/Direct/API/RemoteApiCommandHandler.cs
+++ ICD.Connect.Krang/Remote/Direct/API/RemoteApiCommandHandler.cs
@@ -122,6 +122,7 @@ namespace ICD.Connect.Krang.Remote.Direct.API
 			RemoteApiReply reply = new RemoteApiReply
 			{
 				MessageId = Guid.NewGuid(),
+				MessageTo = hostInfo,
 				Command = eventArgs.Data
 			};
 
