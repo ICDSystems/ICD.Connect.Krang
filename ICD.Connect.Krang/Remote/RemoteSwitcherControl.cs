393d087624e0cb660e883605b7173114c531c3d7
Jack Kanarish <jack.kanarish@wedoresi.com>
2019-02-21 10:53:11 -0500

    Telemetry saftey commit, begin adding switcher telemetry

--- ICD.Connect.Krang/Remote/RemoteSwitcherControl.cs
+++ ICD.Connect.Krang/Remote/RemoteSwitcherControl.cs
@@ -216,6 +216,59 @@ namespace ICD.Connect.Krang.Remote
 			return m_Cache.SetInputForOutput(output, null, type);
 		}
 
+		public override IEnumerable<string> GetSwitcherVideoInputIds()
+		{
+			return GetInputs().Where(i => i.ConnectionType.HasFlag(eConnectionType.Video))
+			                  .Select(i => string.Format("Remote Video Input {0}", i.Address));
+		}
+
+		/// <summary>
+		/// Gets the Input Name of the switcher (ie Content, Display In)
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<string> GetSwitcherVideoInputNames()
+		{
+			return GetSwitcherVideoInputIds();
+		}
+
+		/// <summary>
+		/// Gets the Input Sync Type of the switcher's inputs (ie HDMI when HDMI Sync is detected, empty when not detected)
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<string> GetSwitcherVideoInputSyncType()
+		{
+			foreach (var input in GetInputs().Where(i => i.ConnectionType.HasFlag(eConnectionType.Video)))
+			{
+				bool syncState = GetSignalDetectedState(input.Address, eConnectionType.Video);
+				if (!syncState)
+				{
+					yield return string.Empty;
+					continue;
+				}
+
+				yield return "Remote Video Input Sync";
+			}
+		}
+
+		/// <summary>
+		/// Gets the Input Resolution for the switcher's inputs (ie 1920x1080, or empty for no sync)
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<string> GetSwitcherVideoInputResolution()
+		{
+			foreach (var input in GetInputs().Where(i => i.ConnectionType.HasFlag(eConnectionType.Video)))
+			{
+				bool syncState = GetSignalDetectedState(input.Address, eConnectionType.Video);
+				if (!syncState)
+				{
+					yield return string.Empty;
+					continue;
+				}
+
+				yield return "Unknown";
+			}
+		}
+
 		/// <summary>
 		/// Gets the input routed to the given output matching the given type.
 		/// </summary>
