961048074d49083062138276746cc989f9b832e2
Chris Cameron <chris.cameron@wedoresi.com>
2019-03-27 11:55:52 -0400

    chore: Updating changelog, incrementing patch version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [11.1.2] - 2019-03-27
 ### Changed
  - Remove ids from room settings when a settings instance is removed from the core settings 
 
--- ICD.Connect.Core/Properties/AssemblyInfo.cs
+++ ICD.Connect.Core/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Core")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Core")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("11.1.1.0")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyVersion("11.1.2.0")]
