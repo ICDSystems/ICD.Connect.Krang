3c33d03c2e774ad0cc08f780dbaed5eb25b3ea64
Chris Cameron <chris.cameron@wedoresi.com>
2019-04-23 11:44:15 -0400

    chore: Updating informational version to match DAV

--- ICD.Connect.Krang/Properties/AssemblyInfo.cs
+++ ICD.Connect.Krang/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("12.1.0.0")]
 
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
 #if SIMPLSHARP
-[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.1.0")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.2.0")]
 #endif
