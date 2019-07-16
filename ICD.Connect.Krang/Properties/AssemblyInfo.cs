b19a0dafd41c0c2cb874f929fbb827664ea8bb0e
Chris Cameron <chris.cameron@wedoresi.com>
2019-07-16 11:08:48 -0400

    chore: Updating changelog, incrementing minor version

--- ICD.Connect.Krang/Properties/AssemblyInfo.cs
+++ ICD.Connect.Krang/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Krang")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
-[assembly: AssemblyVersion("11.4.0.0")]
+[assembly: AssemblyVersion("11.5.0.0")]
 
 [assembly: AssemblyInformationalVersion("1.1.0")]
 #if SIMPLSHARP
