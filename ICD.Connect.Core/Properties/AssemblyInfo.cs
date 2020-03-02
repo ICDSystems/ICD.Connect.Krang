aa9b9b861a3e813a5d53ce6cb8f74e059719f4bf
Chris Cameron <chris.cameron@wedoresi.com>
2020-03-02 12:07:00 -0500

    chore: Updating informational version

--- ICD.Connect.Core/Properties/AssemblyInfo.cs
+++ ICD.Connect.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
 [assembly: AssemblyVersion("14.1.1.0")]
 
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyInformationalVersion("1.6.0")]
 #if SIMPLSHARP
-[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.5.0")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.6.0")]
 #endif
