70ea2aa74a0230b08323fff4396da9341db85a50
Chris Cameron <chris.cameron@wedoresi.com>
2020-05-05 10:07:34 -0400

    refactor: Simplified property telemetry

--- ICD.Connect.Krang/Cores/IKrangCoreExternalTelemetryProvider.cs
+++ ICD.Connect.Krang/Cores/IKrangCoreExternalTelemetryProvider.cs
@@ -1,17 +1,18 @@
 using ICD.Connect.Telemetry;
 using ICD.Connect.Telemetry.Attributes;
+using ICD.Connect.Telemetry.Nodes.External;
 
 namespace ICD.Connect.Krang.Cores
 {
 	public interface IKrangCoreExternalTelemetryProvider : IExternalTelemetryProvider
 	{
-		[StaticPropertyTelemetry("ThemeName")]
+		[PropertyTelemetry("ThemeName", null, null)]
 		string ThemeName { get; }
 
-		[StaticPropertyTelemetry("ThemeVersion")]
+		[PropertyTelemetry("ThemeVersion", null, null)]
 		string ThemeVersion { get; }
 
-		[StaticPropertyTelemetry("ThemeInformationalVersion")]
+		[PropertyTelemetry("ThemeInformationalVersion", null, null)]
 		string ThemeInformationalVersion { get; }
 	}
 }
--- ICD.Connect.Krang/Cores/KrangCoreExternalTelemetryProvider.cs
+++ ICD.Connect.Krang/Cores/KrangCoreExternalTelemetryProvider.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using ICD.Common.Properties;
 using ICD.Common.Utils.Extensions;
 using ICD.Connect.Telemetry;
+using ICD.Connect.Telemetry.Nodes.External;
 using ICD.Connect.Themes;
 
 namespace ICD.Connect.Krang.Cores
