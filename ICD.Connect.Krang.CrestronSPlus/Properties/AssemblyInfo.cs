bb342d200301de62795b13633462c079b1621ff9
Drew Tingen <drew.tingen@icdsystems.com>
2020-08-20 15:29:45 -0400

    feat: Adding Krang.CrestronSPlus project, to hold SPlus Krang Bootstrap and related classes

---
+++ ICD.Connect.Krang.CrestronSPlus/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICD.Connect.Krang.CrestronSPlus")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Connect.Krang.CrestronSPlus")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyVersion("15.1.0.0")]
+
