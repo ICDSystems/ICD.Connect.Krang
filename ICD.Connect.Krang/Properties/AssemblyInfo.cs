2138afdda1c65427522e68e6b472de4d0618c305
Chris Cameron <chris.cameron@wedoresi.com>
2021-04-23 14:51:49 -0400

    chore: Incremented informational versions to 1.9

--- ICD.Connect.Krang/Properties/AssemblyInfo.cs
+++ ICD.Connect.Krang/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
 [assembly: AssemblyVersion("15.3.0.0")]
 
-[assembly: AssemblyInformationalVersion("1.8.0")]
+[assembly: AssemblyInformationalVersion("1.9.0")]
 #if SIMPLSHARP
-[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.8.0")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.9.0")]
 #endif
